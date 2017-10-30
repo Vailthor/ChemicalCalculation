1842aa11f59e4d1f96aa99b8e04617bd5f4f3e7f
Vailthor <nloveless96@live.com>
2017-10-30 14:19:36 -0600

    Added Log, Field, Tank, and Chemcial Classes with basic methods and variables

---
+++ app/src/main/java/com/example/nathan/checmicalcalculation/Log.java
@@ -0,0 +1,27 @@
+package com.example.nathan.checmicalcalculation;
+
+import java.util.List;
+
+/**
+ * Created by Nathan on 10/30/2017.
+ */
+
+public class Log {
+    private List<String> log;
+
+    void displayLog() {
+
+    }
+
+    void addToLog(String toAdd) {
+
+    }
+
+    void deleteMostRecent() {
+
+    }
+
+    void getLogEntry() {
+
+    }
+}
