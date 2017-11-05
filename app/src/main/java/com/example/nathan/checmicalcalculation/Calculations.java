1f94ddddba35cc10ffaae2c5563aef5cb86b74a5
Dylan Pratt <dylanjohn0506@gmail.com>
2017-11-05 00:57:38 -0600

    Dylan added the Calculations class

---
+++ app/src/main/java/com/example/nathan/checmicalcalculation/Calculations.java
@@ -0,0 +1,19 @@
+package com.example.nathan.checmicalcalculation;
+
+/**
+ * Created by Dylan Pratt on 11/5/2017.
+ */
+// This class will house the math and logic we need the app to preform
+public class Calculations {
+
+
+    double acresAppliedPerTank(int tanksize, int liquidAppliedPerAcre) {
+        double acresApplied = 0;
+        return acresApplied;
+    }
+    //acres applied per tank
+    double TotalGallonsPerTank(double acresApplied, double ChemRatePerAcre) {
+        double TotalGallonsPerTank =0;
+        return TotalGallonsPerTank;
+    }
+}
