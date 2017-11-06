6a608ee5e6225e346e3eb1c7dfe08cca95ed38b7
Dylan Pratt <dylanjohn0506@gmail.com>
2017-11-06 14:22:05 -0700

    Dylan's Layout changes, Added a Gallons per acre input and added some comments

--- app/src/main/java/com/example/nathan/checmicalcalculation/Calculations.java
+++ app/src/main/java/com/example/nathan/checmicalcalculation/Calculations.java
@@ -8,12 +8,26 @@ public class Calculations {
 
 
     double acresAppliedPerTank(int tanksize, int liquidAppliedPerAcre) {
-        double acresApplied = 0;
+        double acresApplied = tanksize/liquidAppliedPerAcre;
         return acresApplied;
+        //Tank Size/ Liquid applied per acre = acres applied per tank
+
     }
     //acres applied per tank
     double TotalGallonsPerTank(double acresApplied, double ChemRatePerAcre) {
-        double TotalGallonsPerTank =0;
+        double TotalGallonsPerTank =acresApplied * ChemRatePerAcre;
         return TotalGallonsPerTank;
+
+        //acres applied * chemical rate per acre = total chemicals per tank
+    }
+
+    double ouncesToGallons(double ounces) {//converts ounces to gallons
+        double gallons = ounces/128;
+        return gallons;
+    }
+
+    double gallonsToOunces(double gallons) {//converts ounces to gallons
+        double ounces = gallons/128;
+        return ounces;
     }
 }
